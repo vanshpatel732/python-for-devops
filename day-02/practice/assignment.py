33b87a293b65939fe44bf2b111efc8f170244ba4
Vansh Patel <vanshpat@umich.edu>
2026-03-16 00:08:00 -0400

    finished assignment

--- day-02/practice/assignment.py
+++ day-02/practice/assignment.py
@@ -1,15 +1,31 @@
 import requests
+import json
 
-response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo")
+url = "https://api.frankfurter.dev/v1/"
+base = "USD"
+query = f"latest?base={base}"
 
-print(type(response.json()))
-
+response = requests.get(url+query)
 #print(response.json())
 data = response.json()
-for (key, value) in data["Time Series (5min)"].items():
-    print("timestamp:", key)
 
-    if key == "2026-03-13 11:25:00":
-        open1 = value["1. open"]
+print(f"The currect exchange rate of {base} and INR is ", data["rates"]["INR"])
+# I am going to try to find INR or whatever rates that user wants conversion 
+input1 = input("Input the symbol you want to convert from ")
+input2 = input("Input the symbol you want to convert to ")
 
-print(f"The stock opened at {open1}")
+rate1 = data["rates"][input1]
+rate2 = data["rates"][input2]
+final_rate = rate2/rate1
+print(f"The converted rate from {input1} to {input2} is {final_rate}")
+
+data_to_save = {
+    "input1" : input1,
+    "input2" : input2,
+    "rate1" : rate1,
+    "rate2" : rate2,
+    "converted rate": final_rate,
+}
+
+with open ('output.json', 'w') as json_file:
+    json.dump(data_to_save, json_file)
